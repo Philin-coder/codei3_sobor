a6fb01b81f413c3f8e91d9db9c376bad495a3ea8
Alexey <80147483+Philin-coder@users.noreply.github.com>
2024-09-18 15:23:24 +0300

    fix template remove comments from index

--- application/controllers/main_con.php
+++ application/controllers/main_con.php
@@ -3,10 +3,9 @@ if (! defined ('BASEPATH')) EXIT ('No direct script access aliwed');
 class main_con extends CI_Controller {
     public function index(){
         $this->load->helper('url');
-        echo("test");
-        // $this->load->view('temp/head');
-        // $this->load->view('temp/header');
-        // $this->load->view('index');
+        
+        $this->load->view('template/head');
+         $this->load->view('index');
         // $this->load->view('temp/footer');
         // $this->load->view('temp/scripter');
 
