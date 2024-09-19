1b36811780ca6233566dedceafbef7dcb6334886
Alexey <80147483+Philin-coder@users.noreply.github.com>
2024-09-19 22:48:12 +0300

    fix head

--- application/views/template/head.php
+++ application/views/template/head.php
@@ -10,22 +10,11 @@
     <link rel="stylesheet" href="<?php echo base_url();?>/public/css/bootstrap-grid.css">
     <link rel="stylesheet" href="<?php echo base_url();?>/public/css/bootstrap-reboot.css">
     <link rel="stylesheet" href="<?php echo base_url();?>/public/css/style.css">
-    <style amp-boilerplate="">body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}
-    @-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}
-    @-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}
-    @-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}
-    @-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}
-    </style>
+    <link rel="stylesheet" href="<?php echo base_url();?>/public/custom_style.css">
     <noscript>
-    <style amp-boilerplate="">
-    body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none} -->
-    </style>
+    <link rel="stylesheet="href="<?php echo base_url();?>/public/customstyle2.css">
     </noscript>
-    
-    
-        <style>
-            body{background-image: url('https://catherineasquithgallery.com/uploads/posts/2021-02/1612767162_15-p-fon-goluboe-nebo-s-oblakami-19.jpg');}
-        </style>
+    <link rel="stylesheet" href="<?php echo base_url();?>/public/fonstyle.css">
          <meta name="csrf-param" content="_csrf-frontend">
          <meta name="csrf-token" content="rufNjNmfaRuKJ-ssgba1NeE69mEJj3aI0QWIBDjgdkDc0YLLjMY6Tv4fmX_jwfJlh0O3J37HEOqjYtdDbLM5cg==">
          
