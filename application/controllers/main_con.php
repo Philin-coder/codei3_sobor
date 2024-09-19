4d6372763f55374b953e9c36f761359dc27e48c5
Alexey <80147483+Philin-coder@users.noreply.github.com>
2024-09-19 21:42:57 +0300

    fix style

--- application/controllers/main_con.php
+++ application/controllers/main_con.php
@@ -3,13 +3,12 @@ if (! defined ('BASEPATH')) EXIT ('No direct script access aliwed');
 class main_con extends CI_Controller {
     public function index(){
         $this->load->helper('url');
+        $this->load->model('unews_model');
         $this->load->view('template/head');
         $this->load->view('index');
-        $this->db->load->model('unews_model');
         $data['unews']=$this->unews_model->sel_grid_data();
-        $this->load->view('index',$data);
-        $this->load->view('temp/indfooter');
-        $this->load->view('temp/scripter');
+        //  $this->load->view('index',$data);
+         $this->load->view('template/scripter');
 
     }
 }
---
+++ application/views/template/scripter.php
@@ -0,0 +1,18 @@
+        <script src="<?php echo base_url();?>/public/js/cdn_js/v0.js"async="async"></script>
+        <script src="<?php echo base_url();?>/public/js/cdn_js/amp-iframe-0.1.js" async="async" custom-element="amp-iframe"></script>
+         <script src="<?php echo base_url();?>/public/js/cdn_js/amp-lightbox-0.1.js" async" custom-element="amp-lightbox"></script>
+         <script src="<?php echo base_url();?>/public/js/cdn_js/amp-list-0.1.js" async="async" custom-element="amp-list"></script>
+         <script src="<?php echo base_url();?>/public/js/cdn_js/amp-mustache-0.2.js" async="async" custom-template="amp-mustache"></script>
+         <script src="<?php echo base_url();?>/public/js/cdn_js/amp-bind-0.1.js" async="async" custom-element="amp-bind"></script>
+         <script src="<?php echo base_url();?>/public/js/cdn_js/amp-carousel-0.1.js" async="async" custom-element="amp-carousel"></script>        
+         <script src="<?php echo base_url();?>/public/js/cdn_js/amp-analytics-0.1.js" async="async" custom-element="amp-analytics"></script>
+         <script src="<?php echo base_url();?>/public/js/cdn_js/jquery@3.2.1/dist/jquery.min.js" type="text/javascript"></script>
+         <script src="<?php echo base_url();?>/public/js/cdn_js/jquery.maskedinput@1.4.1/src/jquery.maskedinput.min.js" type="text/javascript"></script><script type="text/javascript">
+      <script type="text/javascript">
+      $(document).ready( function () {
+          $('#example').DataTable();
+      } );
+    </script>
+  
+    </body>
+</html>
